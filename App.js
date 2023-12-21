f69326bf0616f6e8f0b6740df5253a6e4870ed4a
padermo <fabioaeh96@gmail.com>
2023-12-20 19:45:38 -0500

    agregando codigo de actualizacion app

--- App.js
+++ App.js
@@ -1,7 +1,26 @@
+import { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Updates from 'expo-updates'
 
 export default function App() {
+  async function onFetchUpdate(){
+    try {
+      const update = await Updates.checkForUpdateAsync();
+
+      if(update.isAvailable){
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+      }
+    } catch (error) {
+      alert(`Error: ${error}`)
+    }
+  }
+
+  useEffect(()=>{
+    onFetchUpdate();
+  },[])
+
   return (
     <View style={styles.container}>
       <Text>Subida</Text>
